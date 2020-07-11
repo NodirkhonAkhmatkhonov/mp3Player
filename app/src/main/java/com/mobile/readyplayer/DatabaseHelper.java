a4e2e1ef714fba4c23a747033e5c6f77670044fe
NodirkhonAkhmatkhonov <nodirxona98@gmail.com>
2020-07-11 05:03:12 +0100

    Last Successfull before DB 3rd attempt to commit

--- app/src/main/java/com/mobile/readyplayer/DatabaseHelper.java
+++ app/src/main/java/com/mobile/readyplayer/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.mobile.readyplayer;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,21 +9,31 @@ import android.support.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String TAG = "DatabaseHelper";
-    public static final String TABLE = "songs_table";
+    public static final String TABLE_NAME = "songs_table";
     public static final String COL1 = "ID";
-//    public static final String COL2 = "path";
+    public static final String COL2 = "playlistName";
+    public static final String COL3 = "pathOfSong";
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL2 + " TEXT)";
+        db.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean addPlaylist() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL2, );
     }
 }
